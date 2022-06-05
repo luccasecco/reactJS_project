f158eecfe3cb7c48da04a41ae473b686d637e3a2
luccasecco <101674470+luccasecco@users.noreply.github.com>
2022-06-05 18:09:26 -0300

    Add files via upload

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logoImg from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header ({ onOpenNewTransactionModal }: HeaderProps) {
+  return (
+    <Container>
+      <Content>
+        <a href="/"><img src={logoImg} alt="dt money" /></a>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  )
+}
